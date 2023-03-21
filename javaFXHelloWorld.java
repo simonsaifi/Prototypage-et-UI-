0262f2f5135f218bc280882b16f711bbf7629589
Simon Saifi <64272731+simonsaifi@users.noreply.github.com>
2023-03-21 16:44:16 +0100

    Create javaFXHelloWorld.java

---
+++ javaFXHelloWorld.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.Label;
+
+public class JavaFXHelloWorld extends Application{
+  public static void main(String[] args){
+    Application.launch(JavaFXHelloWorld.class, args);
+  }
+
+  @Override
+  public void start(Stage stage) {
+    HBox hbox = new HBox();
+    Scene scene = new Scene(hbox);
+    hbox.getChildren().add(new Label("Hello, World"));
+
+    stage.setScene(scene);
+    stage.setTitle("Hello");
+    stage.show();
+  }
+}
